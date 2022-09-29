6486ab5909928590214377c7367a9d0a6e6c9714
Danny Yu <tiramisu@Dannys-MBP.home>
2022-09-28 22:36:45 -0400

    add style

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const url = 'https://rickandmortyapi.com/api/'
-
 let nextButton = document.querySelector('.nextButton')
 let prevButton = document.querySelector('.prevButton')
+let tabButton = document.querySelectorAll('.tabButton')
+let defaultHead = document.querySelector('.default')
 let pageDisplay = document.querySelector('h3')
 let pageNum = 1
 
+//Header Tab
+defaultHead.style.display = "block";
+tabButton.forEach(tabButton => tabButton.addEventListener("click", function () {
+  let tabHead = document.querySelectorAll(".tabHead")
+  tabHead.forEach((tabHead) => {
+    console.log(tabHead)
+    tabHead.style.display = "none";
+  })
+  document.getElementById(tabButton.innerText).style.display = "block";
+}))
+
+//Character Box Function
 function imgList(arr) {
-  let characters = document.querySelector(".character")
+  let characters = document.querySelector(".characterBox")
   characters.innerText = ""
   arr.forEach((char) => {
     let img = document.createElement("img")
@@ -16,13 +29,15 @@ function imgList(arr) {
   });
 }
 
+//Next Button
 nextButton.addEventListener('click', () => {
   if (pageNum < 42) {
     pageNum = pageNum + 1
   } else {
     pageNum = 1
   }
-  pageDisplay.innerText = pageNum 
+  pageDisplay.innerText = pageNum
+
   fetch(url + `character?page=${pageNum}`)
     .then(response => {
       return response.json();
@@ -32,6 +47,7 @@ nextButton.addEventListener('click', () => {
     })
 })
 
+//Previous Button
 prevButton.addEventListener('click', () => {
   if (pageNum > 1) {
     pageNum = pageNum - 1
@@ -47,4 +63,3 @@ prevButton.addEventListener('click', () => {
       imgList(response.results)
     })
 })
-
